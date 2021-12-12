bb2fe6f2933c421ca9a849250fa494d62c510a15
IsabelGK <ikaspriskie@gmail.com>
2021-12-11 19:13:53 -0500

    update avermatrix attrs and help

--- source/projects/ar.avermatrix_tilde/ar.avermatrix_tilde.cpp
+++ source/projects/ar.avermatrix_tilde/ar.avermatrix_tilde.cpp
@@ -5,7 +5,7 @@ using namespace c74::min;
 class avermatrix : public object<avermatrix>, public vector_operator<> {
 public:
 	MIN_DESCRIPTION {"turns averaging into a full-featured EQ"};
-	MIN_TAGS {"audio, effect"};
+	MIN_TAGS {"audio, effect, filter"};
 	MIN_AUTHOR {"Isabel Kaspriskie"};
 
 	inlet<> in1 {this, "(signal) Input1"};
@@ -13,9 +13,9 @@ public:
 	outlet<> out1 {this, "(signal) Output1", "signal"};
 	outlet<> out2 {this, "(signal) Output2", "signal"};
 
-	attribute<number, threadsafe::no, limit::clamp> A {this, "Average", 0.0, range {0.0, 1.0} };
-	attribute<number, threadsafe::no, limit::clamp> B {this, "Depth", 0.0, range {0.0, 1.0} };
-	attribute<number, threadsafe::no, limit::clamp> C {this, "Inv/Wet", 1.0, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> A {this, "average", 0.0, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> B {this, "depth", 0.0, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> C {this, "mix", 1.0, range {0.0, 1.0} };
 
 	message<> dspsetup {this, "dspsetup",
 		MIN_FUNCTION {
