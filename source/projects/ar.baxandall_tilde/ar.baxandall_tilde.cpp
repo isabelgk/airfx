47cf90fb47795e556093c116956b1c4b9f139db1
Isabel Kaspriskie <isabel@cycling74.com>
2021-12-13 16:48:38 -0500

    update baxandall attrs and help

--- source/projects/ar.baxandall_tilde/ar.baxandall_tilde.cpp
+++ source/projects/ar.baxandall_tilde/ar.baxandall_tilde.cpp
@@ -5,7 +5,7 @@ using namespace c74::min;
 class baxandall : public object<baxandall>, public vector_operator<> {
 public:
 	MIN_DESCRIPTION {"a killer general-purpose EQ"};
-	MIN_TAGS {"audio, effect"};
+	MIN_TAGS {"audio, effect, filter"};
 	MIN_AUTHOR {"Isabel Kaspriskie"};
 
 	inlet<> in1 {this, "(signal) Input1"};
@@ -13,9 +13,9 @@ public:
 	outlet<> out1 {this, "(signal) Output1", "signal"};
 	outlet<> out2 {this, "(signal) Output2", "signal"};
 
-	attribute<number, threadsafe::no, limit::clamp> A {this, "Treble", 0.5, range {0.0, 1.0} };
-	attribute<number, threadsafe::no, limit::clamp> B {this, "Bass", 0.5, range {0.0, 1.0} };
-	attribute<number, threadsafe::no, limit::clamp> C {this, "Output", 0.5, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> A {this, "treble", 0.5, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> B {this, "bass", 0.5, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> C {this, "output", 0.5, range {0.0, 1.0} };
 
 	message<> dspsetup {this, "dspsetup",
 		MIN_FUNCTION {
