c6a13fa503fa020a8f253de43188c127d4b6736f
Isabel Kaspriskie <isabel@cycling74.com>
2021-12-13 17:03:03 -0500

    update bite attrs and help

--- source/projects/ar.bite_tilde/ar.bite_tilde.cpp
+++ source/projects/ar.bite_tilde/ar.bite_tilde.cpp
@@ -5,7 +5,7 @@ using namespace c74::min;
 class bite : public object<bite>, public vector_operator<> {
 public:
 	MIN_DESCRIPTION {"an unusual edge-maker"};
-	MIN_TAGS {"audio, effect"};
+	MIN_TAGS {"audio, effect, destruction"};
 	MIN_AUTHOR {"Isabel Kaspriskie"};
 
 	inlet<> in1 {this, "(signal) Input1"};
@@ -13,8 +13,8 @@ public:
 	outlet<> out1 {this, "(signal) Output1", "signal"};
 	outlet<> out2 {this, "(signal) Output2", "signal"};
 
-	attribute<number, threadsafe::no, limit::clamp> A {this, "Bite", 0.5, range {0.0, 1.0} };
-	attribute<number, threadsafe::no, limit::clamp> B {this, "Output Level", 1.0, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> A {this, "bite", 0.5, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> B {this, "output", 1.0, range {0.0, 1.0} };
 
 	message<> dspsetup {this, "dspsetup",
 		MIN_FUNCTION {
