ea2b11c01e3f089c80355b79e70d8a3087925f22
IsabelGK <ikaspriskie@gmail.com>
2021-12-12 22:10:56 -0500

    update basskit attrs and help

--- source/projects/ar.basskit_tilde/ar.basskit_tilde.cpp
+++ source/projects/ar.basskit_tilde/ar.basskit_tilde.cpp
@@ -5,7 +5,7 @@ using namespace c74::min;
 class basskit : public object<basskit>, public vector_operator<> {
 public:
 	MIN_DESCRIPTION {"A centered bass reinforcement with subs fill. Clean and controllable."};
-	MIN_TAGS {"audio, effect"};
+	MIN_TAGS {"audio, effect, saturation"};
 	MIN_AUTHOR {"Isabel Kaspriskie"};
 
 	inlet<> in1 {this, "(signal) Input1"};
@@ -13,10 +13,10 @@ public:
 	outlet<> out1 {this, "(signal) Output1", "signal"};
 	outlet<> out2 {this, "(signal) Output2", "signal"};
 
-	attribute<number, threadsafe::no, limit::clamp> A {this, "Drive", 0.5, range {0.0, 1.0} };
-	attribute<number, threadsafe::no, limit::clamp> B {this, "Voicing", 0.5, range {0.0, 1.0} };
-	attribute<number, threadsafe::no, limit::clamp> C {this, "BassOut", 0.5, range {0.0, 1.0} };
-	attribute<number, threadsafe::no, limit::clamp> D {this, "SubOut", 0.5, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> A {this, "drive", 0.5, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> B {this, "voicing", 0.5, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> C {this, "bass", 0.5, range {0.0, 1.0} };
+	attribute<number, threadsafe::no, limit::clamp> D {this, "sub", 0.5, range {0.0, 1.0} };
 
 	message<> dspsetup {this, "dspsetup",
 		MIN_FUNCTION {
